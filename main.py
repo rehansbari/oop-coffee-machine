4838c0bd4944fe6556f048d290199ac2aef4ecb1
Rehan Bari <rehansbari@gmail.com>
2022-05-10 11:47:02 -0600

    Final code

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee = CoffeeMaker()
+#item = MenuItem()
+admin_function = MoneyMachine()
+
+user_coffee_choice = ""
+while user_coffee_choice != "off":
+    user_coffee_choice = input(f"What would you like? {menu.get_items()}: ")
+    if user_coffee_choice == "report":
+        coffee.report()
+        admin_function.report()
+    else:
+        drink = menu.find_drink(user_coffee_choice)
+        if coffee.is_resource_sufficient(drink) and admin_function.make_payment(drink.cost):
+            coffee.make_coffee(drink)
